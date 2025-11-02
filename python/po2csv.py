55325bf6ef2ca62b81c033a4306097cff56adf70
S-Katagiri <katagiri.stsh@gmail.com>
2025-11-02 21:47:33 +0900

    applied with multiple languages

--- python/po2csv.py
+++ python/po2csv.py
@@ -1,20 +1,28 @@
 from pathlib import Path
 
 import polib
-
+import warnings
 from modules.pofile import po2pddf
 
-dp_text = Path("../text")
+dp_input = Path("input")
+dp_output = Path("intermed")
 
-fp_output = dp_text.joinpath("out.csv")
-fp_po = dp_text.joinpath("vein0.po")
+
+def convertpo2csv(fp: Path) -> bool:
+    lang = fp.parent.name
+    print(f"reading {lang} files: {fp}")
+    fp_output = dp_output.joinpath(f"{lang}/vein0.csv")
+    pof = polib.pofile(fp, encoding="utf-8")
+    df = po2pddf(pof)
+    if not fp_output.parent.exists():
+        fp_output.parent.mkdir(parents=True)
+    df.to_csv(fp_output, index=False)
+    return True
 
 
 def main():
-    pof = polib.pofile(fp_po, encoding="utf-8")
-    df = po2pddf(pof)
-    print(df)
-    df.to_csv(fp_output, index=False)
+    for fp_po in dp_input.rglob("*.po"):
+        convertpo2csv(fp_po)
 
 
 if __name__ == "__main__":
