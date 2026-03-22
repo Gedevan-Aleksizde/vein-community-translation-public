ab190d54d0c3da4937b727ed3da63fd93b1f7b26
S-Katagiri <katagiri.stsh@gmail.com>
2026-03-22 17:36:01 +0900

    英語版の上書きを廃止する

--- bin/pack.bat
+++ bin/pack.bat
@@ -14,7 +14,6 @@ ECHO working dir=%CD%
 FOR /D %%m IN (..\rawassets\content\*) DO (
   ECHO ----- packing %%~nm language files... ------
   TYPE nul > tmp\pack-%%~nm.txt
-  ECHO "%ROOTDIR%\content\%%~nm\Game.locres" "../../../Vein/Content/Localization/Game/en/Game.locres" >> "%ROOTDIR%\tmp\pack-%%~nm.txt"
   ECHO "%ROOTDIR%\content\%%~nm\Game2.locres" "../../../Vein/Content/Localization/Game/%%~nm/Game.locres" >> "%ROOTDIR%\tmp\pack-%%~nm.txt"
   "%UNREALBIN64%\UnrealPak.exe" -create="%ROOTDIR%\tmp\pack-%%~nm.txt" "%RELEASEDIR%\Vein\Content\Paks\~zzzVein-%%~nm.pak"
 
